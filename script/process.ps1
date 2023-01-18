241bb666182dd2ffe8674746e6de55e2a4153250
nogizakapython <92526281+nogizakapython@users.noreply.github.com>
2023-01-18 11:03:30 +0900

    Modefy20230118Morning

---
+++ script/process.ps1
@@ -0,0 +1,38 @@
+﻿#############################################
+#          chromeプロセス取り出し処理             #
+#          新規作成:2023/1/18                #
+#############################################
+
+$LIST_DIR = "c:\script\file\"
+$dat = Get-date -Format "yyyyMMdd"
+$str = "slack"
+
+Test-Path $LIST_DIR$dat.txt
+
+if ($? -eq "True") {
+    echo "すでにファイルが存在します"
+} else {
+    echo "ファイルを作成します"
+    New-item $LIST_DIR$dat.txt
+}
+
+Get-Process >> $LIST_DIR$dat.txt
+
+
+if ($? -eq "True"){
+    echo "OK"
+} else {
+    echo "NG"
+}
+
+$count = (Get-Process | Select-String -Pattern $str |  Measure-Object).Count >> $LIST_DIR$dat.txt
+
+
+if ($? -eq "True"){
+    echo "OK"
+} else {
+    echo "NG"
+}
+
+
+         
