6bfe4043fca3000abe59fe24fa80f21037ab14f5
nogizakapython <92526281+nogizakapython@users.noreply.github.com>
2023-01-27 11:40:01 +0900

    Modefy20230127Morning
    Create Powershell directory

---
+++ powershell/test1.ps1
@@ -0,0 +1,39 @@
+﻿#########################################
+########## Powershell  ##################
+######### 1から10までの数字を当てる######
+#########################################
+
+
+
+function read(){
+    $input = Read-Host("1から10までの整数を入力してください")
+    return $input
+}
+$ans = Get-Random -Minimum 1 -Maximum 10
+$input1 = 0
+$cnt = 0
+$sw = 0
+
+while ($cnt -lt 3){
+    $input1 = read    
+    if ($input1 -lt 1 -and $input1 -gt 10)  {
+        Write-Host "1から10までの整数を入力してください"
+    } else {
+       $cnt++
+       if($input1 -eq $ans){
+            Write-Host("${cnt}回目で正解です")
+            break
+       } else {
+            Write-Host("不正解です")
+            if ($cnt -eq 3){
+                $sw = 1
+                Write-Host("${cnt}回不正解でした")
+                break
+            }
+       }
+    }
+}        
+
+if($sw = 1){
+    Write-Host("正解は${ans}です")
+}
