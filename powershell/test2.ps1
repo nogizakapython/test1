944dbb296c9d78d1312a56c71ac17ce4bdacbaaf
nogizakapython <92526281+nogizakapython@users.noreply.github.com>
2023-01-31 14:08:55 +0900

    Modefy20230131Afternoon

---
+++ powershell/test2.ps1
@@ -0,0 +1,17 @@
+﻿$str1 = "AAA"
+$str2 = "BAB"
+
+$array1 = $str1.ToCharArray()
+$array2 = $str2.ToCharArray()
+
+$num = 3
+$b_cnt = 0
+for($i=0;$i -lt $num ; $i++){
+    if($array1[$i] -eq $array2[$i]){
+        Write-Host("bingo")
+        $b_cnt++
+    } else {
+        Write-Host("ng")
+    }
+}
+Write-Host("${b_cnt}文字正解")
