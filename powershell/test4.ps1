b380442e84b484b53535f190aca844c6df9160fe
nogizakapython <92526281+nogizakapython@users.noreply.github.com>
2023-02-01 08:43:52 +0900

    Modefy20230201Morning

---
+++ powershell/test4.ps1
@@ -0,0 +1,25 @@
+﻿# 入力した文字の配列を作成する。
+
+$num = 3
+$input_array = @()
+
+
+
+#####正解文字列作成処理
+function str_input {
+    for($i=0;$i -lt $num;$i++){
+        $msg = Read-Host("A,B,Cの文字1文字を入力してください")
+        if($msg -eq "A"){
+            $input_array +=  $msg
+        } elseif ($msg -eq "B"){
+            $input_array +=  $msg
+        } elseif ($msg -eq "C"){
+            $input_array += $msg
+        }
+    }
+}
+
+str_input
+foreach($str in $input_array){
+    Write-Host($str)
+}
