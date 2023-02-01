0304096a136e4380094bebecb5a64ccdee66edb8
nogizakapython <92526281+nogizakapython@users.noreply.github.com>
2023-02-01 15:42:54 +0900

    Modefy20230201Afternoon

--- powershell/mojiate.ps1
+++ powershell/mojiate.ps1
@@ -1,10 +1,12 @@
 ﻿##########################
 ##########################
-####  配列テスト     #####
+####  文字当てクイズ     #####
+####  新規作成 2023/1/31 ##
+####  修正    2023/2/1  ##
 ##########################
 
 
-$num = 3
+$num = 4
 $i = 0
 $j = 1
 
@@ -13,15 +15,19 @@ $j = 1
 
 function ans_str {
     while($true){
-        $random = Get-Random -Minimum 1 -Maximum 4
+        $random = Get-Random -Minimum 1 -Maximum 5
         if ($random -eq 1){
             $msg = $msg + "A"
         } elseif ($random -eq 2) {
             $msg = $msg + "B"
         } elseif ($random -eq 3) {
             $msg = $msg + "C"
+        } elseif ($randon -eq 4) {
+            $msg = $msg + "D"
+        } else {
+            $msg = $msg + "C"
         }
-        if($i -eq 2){
+        if($i -eq 3){
             return $msg
             break
         }
@@ -33,17 +39,17 @@ function ans_str {
 #####入力文字列作成処理
 function str_input {
     while($true){
-        $msg = Read-Host("A,B,Cの3文字を使ったアルファベット3語を入力してください")
-        $flag = $msg -match '[A-C][A-C][A-C]'
+        $msg = Read-Host("A,B,C,Dの4文字を使ったアルファベット4語を入力してください")
+        $flag = $msg -match '[A-D][A-D][A-D][A-D]'
         if($flag -eq "True") {
             $count = $msg.Length
-            if ($count -eq 3){
+            if ($count -eq $num){
                 return $msg
             } else {
-                Write-Host("3文字にしてください")
+                Write-Host("4文字にしてください")
             }
         } else {
-            Write-Host("A,B,C以外入力しないでください")
+            Write-Host("A,B,C,D以外入力しないでください")
         }
 
     }
@@ -53,16 +59,25 @@ function str_input {
 
 ####################
 #####メイン処理########
+$ans_str = ans_str
+
+##正解まで繰り返す
 while($true) {
-    $ans_str = ans_str
-    $input = str_input
-    if($ans_str -eq $input){
+    $input_str = str_input
+    if($ans_str -eq $input_str){
         Write-Host("${j}回目で文字列がそろって正解です")
         break
     } else {
         Write-Host("${j}回目、不正解です")
+        $ans_array = $ans_str.ToCharArray()
+        $inp_array = $input_str.ToCharArray()
+        for($k=0;$k -lt $num;$k++){
+            if ($ans_array[$k] -eq $inp_array[$k]){
+                $p = $k + 1
+                Write-Host("左から${p}文字目が正解です")
+            }
+        }
         $j++
     }
 }
-
 
---
+++ powershell/test6.ps1
@@ -0,0 +1,9 @@
+﻿$msg = Read-Host("文字を入力してください")
+
+$msg2 = $msg.ToCharArray()
+$num = $msg2.Count
+
+for($i=0;$i -lt $num;$i++){
+    echo $msg2[$i]
+}
+
