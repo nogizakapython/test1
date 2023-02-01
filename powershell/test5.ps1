e743f28223d97e520a1fe9ce3c41ae86d4efd06b
nogizakapython <92526281+nogizakapython@users.noreply.github.com>
2023-02-01 12:52:40 +0900

    Modefy20230201noon

---
+++ powershell/test5.ps1
@@ -0,0 +1,22 @@
+﻿$msg = ""
+$num = 3
+function ans_str {
+    for($i=0;$i -lt $num;$i++){
+        $random = Get-Random -Minimum 1 -Maximum 4
+        if ($random -eq 1){
+            $msg = $msg + "A"
+        } elseif ($random -eq 2) {
+            $msg = $msg + "B"
+        } elseif ($random -eq 3) {
+            $msg = $msg + "C"
+        }
+        if($i -eq 2){
+            return $msg
+            break
+        }
+    }
+}
+
+$ans_str = ans_str
+echo $ans_str
+
