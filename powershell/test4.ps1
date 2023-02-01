ff80ff4a083292611a3be6a842f0dfee7372fcb2
nogizakapython <92526281+nogizakapython@users.noreply.github.com>
2023-02-01 12:45:05 +0900

    Modefy20230201Morning

--- powershell/test4.ps1
+++ powershell/test4.ps1
@@ -1,25 +1,27 @@
 ﻿# 入力した文字の配列を作成する。
 
-$num = 3
-$input_array = @()
-
 
 
 #####正解文字列作成処理
 function str_input {
-    for($i=0;$i -lt $num;$i++){
-        $msg = Read-Host("A,B,Cの文字1文字を入力してください")
-        if($msg -eq "A"){
-            $input_array +=  $msg
-        } elseif ($msg -eq "B"){
-            $input_array +=  $msg
-        } elseif ($msg -eq "C"){
-            $input_array += $msg
+    while($true){
+        $msg = Read-Host("A,B,Cの3文字を使ったアルファベット3語を入力してください")
+        $flag = $msg -match '[A-C][A-C][A-C]'
+        if($flag -eq "True") {
+            $count = $msg.Length
+            if ($count -eq 3){
+                return $msg
+            } else {
+                Write-Host("3文字にしてください")
+            }
+        } else {
+            Write-Host("A,B,C以外入力しないでください")
         }
+
     }
+       
 }
 
-str_input
-foreach($str in $input_array){
-    Write-Host($str)
-}
+$input = str_input
+echo $input
+
