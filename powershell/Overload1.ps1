7ade37bb7d5e3dd1040e04453b4c2d011d431186
nogizakapython <92526281+nogizakapython@users.noreply.github.com>
2023-02-09 13:35:33 +0900

    Modefy20230209afternoon

---
+++ powershell/Overload1.ps1
@@ -0,0 +1,29 @@
+﻿class Testclass1 {
+    #int型
+    [int] $iData
+    #String型
+    [string] $strData
+
+    #数値を加算する
+    [int] Add([int] $Indata) {
+        $this.iData += $Indata
+        return $this.iData
+    }
+    #文字列を追加する
+    [string] Add ( [string] $Indata ) {
+        $this.strData += $Indata
+        return $this.strData
+    }
+}
+
+#インスタンス化
+$TestObject1 = New-Object TestClass1
+
+#数値計算
+$TestObject1.Add(10)
+$TestObject1.Add(20)
+
+#文字列操作
+$TestObject1.Add("This is ")
+$TestObject1.Add("Mukai Shutoku.")
+
---
+++ powershell/classtest2.ps1
@@ -0,0 +1,19 @@
+﻿class Test2
+{
+    [int] $NUM
+    [int] Add([int] $Indata){
+        $this.NUM += $Indata
+
+        #内部データ
+        return $this.NUM
+    }
+}
+
+#インスタンス化
+$TestObject2 = New-Object Test2
+
+#10 を加算
+$TestObject2.Add(10)
+#30を加算
+$TestObject2.Add(30)
+
