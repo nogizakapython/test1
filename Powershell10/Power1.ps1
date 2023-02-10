cf0821cc87c619c9c055052f05e4ac85d5b1c368
nogizakapython <92526281+nogizakapython@users.noreply.github.com>
2023-02-10 13:58:46 +0900

    Modefy20230210Afternoon

---
+++ Powershell10/Power1.ps1
@@ -0,0 +1,30 @@
+﻿class Exam1 {
+    [int]  $num = 1
+    [string] $str = ""
+
+    [int] Power([int] $msg){
+        $this.num *= $msg 
+        return $this.num
+    }
+    [string] Power([string] $msg){
+        for($i=0;$i -lt 2;$i++){
+            $this.str += $msg
+        }
+        return $this.str
+    }
+
+}
+
+#インスタンス化
+$Exam = New-Object Exam1
+
+#数値計算
+$Exam.Power(2)
+$Exam.Power(3)
+
+#文字列操作
+$Exam.Power("Powershell")
+$Exam.Power("Microsoft")
+
+
+
---
+++ Powershell10/classtest1.ps1
@@ -0,0 +1,9 @@
+﻿class Test1 {
+    [string] method1() {
+        return "Powershell test"
+    }
+}
+
+$test1 = New-Object Test1
+
+$test1.method1()
