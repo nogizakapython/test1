20d09ad3941ef8bf5bfb02df3f4e8cd1cf2f58e0
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2023-03-08 13:19:59 +0900

    Modefy20230308Afternoon

---
+++ powershell/TestClass2.ps1
@@ -0,0 +1,54 @@
+﻿# Powershell class Test2
+# 新規作成 2023/3/8
+
+class TestClass1{
+    [int] $ID
+    [String] $Name
+    [String] $Dept
+    
+    #コンストラクタ
+    Disp([int] $ID,[String] $Name,[String] $Dept) {
+        $this.Id = $Id
+        $this.Name = $Name
+        $this.Dept = $Dept
+    }
+    #ToStringメソッド
+    [String] ToString() {
+        return "Id=" + $this.Id + ",Name=" + $this.Name
+    }
+    #Todeptメソッド
+    [String] Todept() {
+        return $this.Name + "は" + $this.Dept + "です。"
+    }
+    #Tocallメソッド
+    [String] Tocall() {
+        return $this.Dept
+    }
+    #Tonameメソッド
+    [String] Toname() {
+        return $this.name
+    }
+
+
+}
+
+# TestClass1を継承して、testClass2を作成する。
+class TestClass2 : TestClass1{
+    [String] Like(){
+        $Data = ([Testclass1]$this).Toname()
+        $Data += "は面白い"
+        return $Data
+    }
+}
+
+
+$T1 = New-Object TestClass1
+$T1.Disp(1,"Powershell","Microsoft")
+$T1.ToString()
+$T1.ToDept()
+
+$T2 = New-Object TestClass2
+$T2.Disp(2,"Python3","Science")
+$T2.ToString()
+$T2.ToDept()
+$T2.Like()
---
+++ powershell/Testclass.ps1
@@ -0,0 +1,9 @@
+﻿class TestClass{
+    [string] hoge(){
+        return "Hello Powershell"
+    }
+}
+
+$Test1 = New-Object TestClass
+
+$Test1.hoge()
