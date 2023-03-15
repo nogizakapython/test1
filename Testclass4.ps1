1b8224a9fbb7b9319150155fe5b8924fea0a3985
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2023-03-15 13:08:24 +0900

    Modefy20230315afternoon

---
+++ Testclass4.ps1
@@ -0,0 +1,46 @@
+﻿# Powershell class Test4
+# 新規作成 2023/3/15
+
+class TestClass4{
+    [int] $ID
+    [String] $Name
+    
+    
+    #コンストラクタ
+    Disp([int] $ID,[String] $Name) {
+        $this.Id = $Id
+        $this.Name = $Name
+    
+    }
+    #ToStringメソッド
+    [String] ToString() {
+        return "Id=" + $this.Id + ",Name=" + $this.Name
+    }
+    #ToNameメソッド
+    [String] ToName() {
+        return "私の名前は" + $this.Name + "です。"
+    }
+    
+
+
+}
+
+# TestClass4を継承して、testClass5を作成する。
+class TestClass5 : TestClass4{
+    [String] Livein(){
+        $Data = ([Testclass4]$this).ToName()
+        $Data += "出身は立川市です"
+        return $Data
+    }
+}
+
+$T1 = New-Object TestClass4
+$T1.Disp(1,"白石麻衣")
+$T1.ToString()
+$T1.ToName()
+
+$T2 = New-Object TestClass5
+$T2.Disp(2,"山下美月")
+$T2.ToString()
+$T2.ToName()
+$T2.Livein()
