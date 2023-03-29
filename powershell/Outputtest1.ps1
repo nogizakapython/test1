02c7b1bbc0f518aaccf9d1ea3f4570a4f9cce10c
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2023-03-29 10:55:43 +0900

    Modefy20230329Morning

---
+++ powershell/Outputtest1.ps1
@@ -0,0 +1,18 @@
+﻿############################################
+#   echo、Write-Host、Write-Outputの違い    ###
+#  新規作成  2023/3/29                    ###
+############################################
+
+
+Write-Host "***** echo コマンドの出力結果"
+echo $PSVersionTable
+
+Write-Host "***** Write-Host コマンドの出力結果"
+Write-Host $PSVersionTable
+
+
+Write-Host "***** Write-Out コマンドにパイプでFormat-Listの出力結果"
+Write-Output $PSVersionTable | Format-List
+
+
+
