b5190fdeaa610434adaa63ea2a714ab7ad0559c0
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2023-04-06 12:00:27 +0900

    Modefy20230406Morning

---
+++ powershell/testClass11.ps1
@@ -0,0 +1,17 @@
+﻿# クラス定義
+class TestClass{
+    [string] test(){
+        return "Hello PowerShell Class !!"
+    }
+}
+
+# インスタンス化
+$TestObject = New-Object TestClass
+
+echo "メンバー"
+$TestObject | Get-Member
+
+echo ""
+
+echo "タイプ"
+$TestObject.GetType()
