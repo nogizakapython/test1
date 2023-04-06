ea5a2624aeb987193314087eb2a6f722364f04d7
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2023-04-06 12:48:35 +0900

    Modefy20230406noon

---
+++ powershell/StrClass1.ps1
@@ -0,0 +1,25 @@
+﻿# クラス定義
+class StrClass1{
+    $name
+    
+    [string] hoge(){
+        return "Hi! Powershell's world"
+    }
+    [string] hoge($name){
+        $this.name = $name
+        return "Hi! " + $name 
+    }
+}
+
+# インスタンス化
+$StrObject = New-Object StrClass1
+
+echo "メンバー"
+$StrObject | Get-Member
+
+
+echo "タイプ"
+$StrObject.GetType()
+
+$StrObject.hoge()
+$StrObject.hoge("nogizakapython")
