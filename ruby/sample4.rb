1c909ccef044fbd7abcac5a65f823ecfc0570189
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2023-05-09 06:41:18 +0900

    Modefy20230509Morning

---
+++ ruby/sample4.rb
@@ -0,0 +1,12 @@
+# 自分の得意な言語で
+# Let's チャレンジ！！
+print("数字を入力してください")
+data = gets
+array1 = data.split(' ')
+A = array1[0].to_i
+B = array1[0].to_i
+if A == 1 or B == 1 then
+  puts 1
+else
+  puts 0
+end
