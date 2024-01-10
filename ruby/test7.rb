d0a5b5b4900116c5bfd6b4816218d813752921ed
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2024-01-11 07:20:18 +0900

    Modefy20240111Morning

---
+++ ruby/test7.rb
@@ -0,0 +1,11 @@
+p 10 + 3
+p 10 * 3
+p 2.4 * 2
+p 10 / 3
+p 10 % 3
+p 10.0 / 3
+p Rational(2,5) + Rational(3,4)
+p 2/5r + 3/4r
+p 52.6.floor
+p 52.6.round
+p 52.6.ceil
---
+++ ruby/test8.rb
@@ -0,0 +1,13 @@
+puts "hello world"
+puts "hell\no worl\td"
+puts 'hell\no worl\td'
+
+puts "price #{3000 * 4}"
+ans = 2000 * 5
+puts "price #{ans}"
+
+name = "manatsu"
+puts "hello #{name}"
+
+puts "hello " + "world"
+puts "=" * 20
