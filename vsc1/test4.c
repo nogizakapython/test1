28e6b761808f7e4627acf3403ac2d043af8349c8
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2024-01-24 11:43:38 +0900

    20240124

---
+++ vsc1/test4.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+  printf("%cは文字です。\n",'A');
+  printf("%dは整数です。\n",123);
+  printf("%fは整数です。\n",3.14);
+
+}
---
+++ vsc1/test5.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+  char str1[] = "nogizaka";
+  printf("%sは文字列です。\n",str1);
+
+
+}
