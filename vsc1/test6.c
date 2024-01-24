7314428710f0cb06a46f6a702df88d671aa78b6b
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2024-01-24 11:48:00 +0900

    Modefy20240124

---
+++ vsc1/test6.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+  printf("%cは文字、%dは整数です。\n",'A',123);
+  printf("%dは整数、%fは小数です。\n",24,2.718);
+
+
+}
---
+++ vsc1/test7.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+  printf("円記号を表示します。:%c \n",'\\');
+  printf("アポストロフィを表示します。:%c \n",'\'');
+
+
+}
