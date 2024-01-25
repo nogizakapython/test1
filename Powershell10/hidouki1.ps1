9a34ef116e22f56dc0c03034af81c01c189cc3e8
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2024-01-25 17:16:51 +0900

    20240125

---
+++ Powershell10/hidouki1.ps1
@@ -0,0 +1,24 @@
+﻿Get-Date
+$music_file = "C:\20220106\powershell\test1.wav"
+$job1 = {
+          for($i=0;$i -lt 2;$i++){
+            sleep 10;
+            "[+] Finished (1)"
+          }
+       
+}
+
+
+$sp = New-Object Media.SoundPlayer $music_file
+
+Start-Job $job1
+
+
+$sp.Play()
+sleep 20
+$sp.Stop()
+
+
+
+Get-Date
+
---
+++ Powershell10/music1.ps1
@@ -0,0 +1,29 @@
+﻿##############################
+######  Morning call      ####
+##############################
+
+$hour1 = (Get-Date).Hour
+$minute1 = (Get-Date).Minute
+$music_file = "C:\20220106\powershell\test1.wav"
+
+Function task1()  {
+    $sp = New-Object Media.SoundPlayer $music_file
+    $sp.Play()
+    return "END"
+
+}
+
+
+if (($hour1 -eq 6) -and ($minute1 -eq 0))
+{
+    Write-Host "Please Get up!"
+    #$sp = New-Object Media.SoundPlayer ./test1.wav
+    #$sp.Play()
+    #$sp.Stop()
+}
+
+
+$job1 = Start-Job -ScriptBlock task1
+Wait-job $job1.Id
+
+$res1 = Receive-Job $job1 -Wait 
