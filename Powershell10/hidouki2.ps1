8af755338ca5884aa6a81f199ea211f0b9c94056
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2024-01-25 17:51:38 +0900

    Modefy20240125

---
+++ Powershell10/hidouki2.ps1
@@ -0,0 +1,20 @@
+﻿# 開始時間の表示
+Get-Date
+# バックグラウンドで動かすジョブ
+$function1 = {
+ Start-Sleep 10
+ write-host "function1終了"
+}
+function function2() {
+ Start-Sleep 10
+ write-host "function2終了"
+}
+
+# バックグラウンドジョブ
+$job1 = Start-Job -ScriptBlock $function1
+# 関数２を呼び出して10秒スリープ
+function2
+# バックグラウンドジョブ終了まで待ちます
+Wait-Job $job1.Id
+# 終了時間の表示
+Get-Date
---
+++ Powershell10/hidouki3.ps1
@@ -0,0 +1,30 @@
+﻿# 開始時間の表示
+Get-Date
+# バックグラウンドで動かすジョブ
+$task1 = {
+ Start-Sleep 20
+ write-host "task1終了"
+}
+
+$task2 = {
+ sleep 3
+ Write-Host "hogehoge"
+ sleep 7
+ write-host "task2終了"
+}
+
+function function3() {
+ Start-Sleep 10
+ write-host "function3終了"
+}
+
+# バックグラウンドジョブ
+$job1 = Start-Job -ScriptBlock $task1
+$job2 = Start-Job -ScriptBlock $task2
+# 関数２を呼び出して10秒スリープ
+function3
+# バックグラウンドジョブ終了まで待ちます
+Wait-Job $job1.Id
+Wait-Job $job2.Id
+# 終了時間の表示
+Get-Date
