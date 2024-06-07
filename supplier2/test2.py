4d8b74815f0e8fb0be7a8375e05f48a6abead32d
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2024-06-07 11:40:40 +0900

    Modefy20240607

--- supplier2/test2.py
+++ supplier2/test2.py
@@ -1,19 +1,29 @@
-weather_1 =""
-count = 0
+################################
+#  ヒアドキュメントテスト
+#  新規作成  2024/6/7
+################################
 
-with open('text1.txt', encoding='utf-8') as f:
-    lines = f.readline()
-    lines = lines.replace("\n",'')
-    while True:
-        if lines == "":
-            break
-        else:
+# メイン関数
+def main():
+    nogi_oshimen = "遠藤さくら"
+    sakura_oshimen = "田村保乃"
+    hinata_oshimen = "正源司陽子"
+    count = 0
+    # ヒアドキュメント用ファイルを開き、テキスト内の変数を代入する   
+    with open('text2.txt', encoding='utf-8') as f:
+        for line in f:
+            line = line.replace("\n",'')
+            oshimen = ""
             if count == 0:
-                weather_1="fine"
+                oshimen = f"""{line.format(nogi_oshimen=nogi_oshimen)}"""
             elif count == 1:
-                weather_1="rain"
-            else:
-                weather_1="snow"        
-        output = f"""{lines.format(weather_1=weather_1)}"""
-        print(output)
+                oshimen = f"""{line.format(sakura_oshimen=sakura_oshimen)}"""   
+            elif count == 2:
+                oshimen = f"""{line.format(hinata_oshimen=hinata_oshimen)}"""      
+            # 1行ごとにヒアドキュメントの中身を表示する。
+            print(oshimen)        
+            count += 1
+
     
+if __name__ == "__main__":
+    main()
