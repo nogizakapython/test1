5b7a1f295175ea14712f2469d8a0aa6b1b2c4b92
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2024-06-11 17:08:32 +0900

    Modefy20240611

---
+++ powershell/pythondoc.ps1
@@ -0,0 +1,6 @@
+﻿$DirPath = "c:\users\takao.hattori\onedrive - accenture\python_asobi"
+$result_file = $DirPath + "\" + "result.txt"
+$array1 = @("sample_dsi","sample_jpit")
+foreach($str in $array1){
+    Write-Host $str
+}
