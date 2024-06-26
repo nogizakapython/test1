0611d2a16bf88d7cf74f7530439b787f530c27da
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2024-06-26 10:48:47 +0900

    Modefy20240625

--- A0025/shiseido.py
+++ A0025/shiseido.py
@@ -48,26 +48,26 @@ driver = webdriver.Chrome()
 
 # Chromeを開いて企業HPにアクセスする
 
-for year in [date5,date6]:
-   target_url = web_url + str(year) + ".html"
+
+target_url = web_url
    
    
-   try:
-      driver.get(target_url)
-      sleep(3)
-      for i in range(1,13):
-         try:
-            xpath_str1 = '//*[@id="ir"]/div[1]/div/div[2]/div/div/div[3]/div/ul/li[' + str(i) + ']/a'
-            element_str1 = driver.find_element(by=By.XPATH,value=xpath_str1)
-            print(element_str1.get_attribute("outerHTML"),file=codecs.open(input_file,'a','utf-8'))
-         except:
-            break    
+try:
+   driver.get(target_url)
+   sleep(3)
+   for i in range(1,31):
+      try:
+         xpath_str1 = '/html/body/div[1]/div/div[2]/div/div/div[3]/div[1]/ul/li[' + str(i) + ']/div'
+         element_str1 = driver.find_element(by=By.XPATH,value=xpath_str1)
+         print(element_str1.get_attribute("outerHTML"),file=codecs.open(input_file,'a','utf-8'))
+      except:
+         break    
           
        
-   except EnvironmentError as e:
-      str100 = e     
-   except:
-      str100 = 1
+except EnvironmentError as e:
+   str100 = e     
+except:
+   str100 = 1
      
 # 画面を閉じる
 driver.quit()
@@ -88,15 +88,20 @@ while True:
         row_count += 1
      else:
         break   
-
-     result1 = re.match('<a href',line1)
-     result2 = re.search('<em>',line1)
-     result3 = re.search('<p>',line1)
-
+     
+     result1 = re.search('<em>',line1)
+     result2 = re.search('<a href',line1)
 
      if result1:
-        w_array1 = line1.split("=")
-        w_urlstr = w_array1[1]
+        w_array1 = line1.split(">")   
+        w_ymdstr = w_array1[1]
+        w_ymdstr = w_ymdstr.replace('</em','')
+        w_ymd = w_ymdstr.replace('.','/')
+      #   print(w_ymd)
+
+     if result2:
+        w_array2 = line1.split("=")
+        w_urlstr = w_array2[1]
         w_urlstr = w_urlstr.replace('target','')
         w_urlstr = w_urlstr.replace('"','')
         url_result = re.match("https",w_urlstr)
@@ -106,20 +111,13 @@ while True:
            w_url = base_url + w_urlstr   
       #   print(w_url)
 
-     if result2:
-        w_array2 = line1.split(">")   
-        w_ymdstr = w_array2[1]
-        w_ymdstr = w_ymdstr.replace('</em','')
-        w_ymd = w_ymdstr.replace('.','/')
-      #   print(w_ymd)
-
-     if result3:
         w_array3 = line1.split(">")
         w_titlestr = w_array3[1]
-        w_title = w_titlestr.replace('</p','')
+        w_titlestr = w_titlestr.replace('</p','')
+        w_title = w_titlestr.replace('</a','')
       #   print(w_title)
 
-        key_word = r"(決算|株主総会|説明会|IR説明会|中期経営計画|報告書|レポート)"
+        key_word = r"(決算|株主総会|説明会|IR説明会|中期経営計画|報告書|レポート|経営|経営戦略)"
         title_result = re.search(key_word,w_title)
         if title_result:
             wb = op.load_workbook(export_file)
