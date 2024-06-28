3905bb74be4f7a0eb7d13803785ae3b87cde560d
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2024-06-28 14:26:15 +0900

    Modefy20240628

---
+++ dotnet/test1/sample2-7.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample207
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int NUMBER = 100;
+            const string STRING = "Hogehoge";
+            Console.WriteLine(NUMBER);
+            Console.WriteLine(STRING);
+        }
+    }
+}
