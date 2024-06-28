80fb1b739dc042e72c6873f543e23a364addc1bc
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2024-06-28 15:20:41 +0900

    Modefy20240628

---
+++ dotnet/test1/Prob2-5.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace Problem2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 4,b = 2;
+            Console.WriteLine("a = {0}",a);
+            Console.WriteLine("b = {0}",b);
+            Console.WriteLine("a + b = {0}",a + b);
+            Console.WriteLine("a - b = {0}",a - b);
+            Console.WriteLine("a * b = {0}",a * b);
+            Console.WriteLine("a / b = {0}",a / b);
+            Console.WriteLine("a % b = {0}",a % b);
+        }
+    }
+}    
