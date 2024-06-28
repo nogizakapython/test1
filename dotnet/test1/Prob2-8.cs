ed3ea8f428bebb60bd89246aeb1ad2e05721e517
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2024-06-28 16:12:45 +0900

    Modefy20240628

---
+++ dotnet/test1/Prob2-8.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace Problem2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("文字列を入力");
+            string str1 = Console.ReadLine();
+            Console.WriteLine("入力した文字列:{0}",str1);
+            
+        }
+    }
+}    
