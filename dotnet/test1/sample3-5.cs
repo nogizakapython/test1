f79544b5e9af07ad1f488f473a8aa25b6b465980
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2024-07-01 13:31:02 +0900

    Modefy20240701

---
+++ dotnet/test1/sample3-5.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace Sample305
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int NUMBER = 100;
+            const string NAME = "大園玲";
+            Console.WriteLine(NUMBER);
+            Console.WriteLine(NAME);
+            Console.WriteLine("{0}のテスト結果は{1}点です",NAME,NUMBER);
+        }
+    }
+}
