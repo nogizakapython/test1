9fdc99991aa6b93bb73176a707d8aecb72fad195
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2024-07-04 16:27:19 +0900

    Modefy20240704

---
+++ dotnet/test1/prob4-5.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace Prob45
+{
+    class Prob45
+    {
+        static void Main(string[] args)
+        {
+            
+            int num = 2;
+            while(num < 10){
+                Console.WriteLine("a = " + num);
+                num += 2;
+            }
+
+            
+            
+        }
+    }
+}
---
+++ dotnet/test1/prob4-6.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace Prob46
+{
+    class Prob46
+    {
+        static void Main(string[] args)
+        {
+            
+            int num = 2;
+            for(int i=num;i<10;i += 2){
+                Console.WriteLine("num = " + i);
+            }
+
+            
+            
+        }
+    }
+}
