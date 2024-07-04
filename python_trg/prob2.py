7e59ec60765c7d839810c45b8f70463fee31ae96
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2024-07-04 16:49:36 +0900

    Modefy20240704

---
+++ python_trg/prob2.py
@@ -0,0 +1,8 @@
+import random
+
+while True:
+    num = random.randint(1,100)
+    print(num)
+    if num % 10 == 0:
+        break
+    
---
+++ python_trg/sample8.py
@@ -0,0 +1,7 @@
+# 数値の入力を求めるプログラム例
+
+print("数字を入力し、「Enter」キーを押してください")
+
+number = int(input())
+
+print(number)
