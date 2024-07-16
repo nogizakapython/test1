60e4be12c67dcb8712a3556e295e3548aa2b2e28
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2024-07-16 15:02:59 +0900

    Modefy20240716

--- dotnet/test1/prob7-3.cs
+++ dotnet/test1/prob7-3.cs
@@ -21,12 +21,19 @@ namespace Problem7_3
             set{ number2 = value;}
             get{ return number2;}
         }
-        public int Add(int number1,int number2){
-            return number1 + number2;
+        public void SetNumber(int number1,int number2)
+        {
+            this.number1 = number1;
+            this.number2 = number2;
+        }
+        public int Add{
+            set { number1 = this.number1;number2 = this.number2;}
+            get {return number1 + number2;}
         }
 
-        public int Sub(int number1,int number2){
-            return number1 - number2;
+        public int Sub{
+            set { number1 = this.number1;number2 = this.number2;}
+            get {return number1 - number2;}
         }
 
     }
@@ -37,10 +44,11 @@ namespace Problem7_3
             Calculation c = new Calculation();
             c.Number1 = 8;    //  一つ目の数をセット
             c.Number2 = 9;    //  二つ目の数をセット
+            c.SetNumber(c.Number1,c.Number2);
             //  二つの数の和を表示
-            Console.WriteLine("{0} + {1} = {2}",c.Number1,c.Number2,c.Add(c.Number1,c.Number2));
+            Console.WriteLine("{0} + {1} = {2}",c.Number1,c.Number2,c.Add);
             //  二つの数の差を表示
-            Console.WriteLine("{0} - {1} = {2}",c.Number1,c.Number2,c.Sub(c.Number1,c.Number2));
+            Console.WriteLine("{0} - {1} = {2}",c.Number1,c.Number2,c.Sub);
         }
     }
 }
