99bde2ff2cfd6f2c60ab22acb01cd794c1649ba7
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2024-07-22 14:03:06 +0900

    Modefy20240722

---
+++ dotnet/test1/prob8-2.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace Problem6_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int a=1;a<=100;a++){
+                for(int b=a;b<=100;b++){
+                    for(int c=b;c<=100;c++){
+                        double sum1 = Math.Pow(a,2) + Math.Pow(b,2);
+                        double sum2 = Math.Pow(c,2);
+                        if(sum1 == sum2){
+                            Console.WriteLine("a={0},b={1},c={2}",a,b,c);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
