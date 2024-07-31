c624a45a17b0a7b94f1de557500c7c37679cdc87
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2024-07-31 16:22:57 +0900

    Modefy20240731

---
+++ dotnet/test1/prob10-1.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace Prob10_1
+{
+    class Sample
+    {   
+        private string str1  = "";
+        public Sample() : this("コンストラクタ")
+        {
+            
+        }
+        
+         
+        public Sample(string msg)
+        {
+            this.str1 = msg;
+            Console.WriteLine(this.str1);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sample s1 = new Sample();
+            Sample s2 = new Sample("foo");
+        }
+    }
+}
