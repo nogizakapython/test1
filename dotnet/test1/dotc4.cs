ffe9954216b4563ba38162f4ea0650525fb628e4
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2024-08-02 16:22:34 +0900

    Modefy20240802

---
+++ dotnet/test1/dotc4.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace dotc4
+{
+    class Dotc4
+    {
+        static void Main(string[] args)
+        {
+            string msg = "Hello C#";
+            Console.WriteLine(msg);
+        }
+    }
+}    
