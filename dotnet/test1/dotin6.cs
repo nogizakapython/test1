673a059cf25b31f7f6e5ec9c29a86b4a1ca912de
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2024-08-05 17:01:08 +0900

    Modefy20240805

---
+++ dotnet/test1/dotin6.cs
@@ -0,0 +1,12 @@
+using System;
+
+class dotin6
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("test1\ntest2");
+        var str1 = "白石麻衣";
+        var str2 = 31;
+        Console.WriteLine("{0}は{1}才です",str1,str2);
+    }
+}
