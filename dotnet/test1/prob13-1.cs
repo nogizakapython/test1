e8769bc9bff86dcf4f4ed3436e50c821246d192b
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2024-08-06 10:56:10 +0900

    Modefy20240806

---
+++ dotnet/test1/prob13-1.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace Prob13_1
+{
+    //  抽象クラス動物
+    abstract class Animal
+    {
+        //  名前
+        protected string name = "";
+        //  吠える
+        public abstract void Bark();
+        //  動く
+        public abstract void Move();
+        //  名前を表示する
+        public void ShowName()
+        {
+            Console.WriteLine("名前:{0}", name);
+        }
+    }
+    //  犬クラス
+    class Dog : Animal
+    {
+        public Dog()
+        {
+            name = "犬";
+        }
+        //  吠える
+        public override void Bark()
+        {
+            Console.WriteLine("ワンワン");
+        }
+        //  動く
+        public override void Move()
+        {
+            Console.WriteLine("歩く");
+        }
+    }
+    class Monkey : Animal
+    {
+        public Monkey()
+        {
+            name = "猿";
+        }
+        //  吠える
+        public override void Bark()
+        {
+            Console.WriteLine("キーキー");
+        }
+        //  動く
+        public override void Move()
+        {
+            Console.WriteLine("木に登る");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Animalクラスのオブジェクトの配列を生成
+            Animal[] animals = new Animal[2];
+            animals[0] = new Dog();     //  犬クラスのインスタンス生成
+            animals[1] = new Monkey();  // 猿クラスのインスタンスの生成
+            for (int i = 0; i < animals.Length; i++)
+            {
+                animals[i].ShowName();
+                animals[i].Move();
+                animals[i].Bark();
+                Console.WriteLine("------------");
+            }
+        }
+    }
+}
