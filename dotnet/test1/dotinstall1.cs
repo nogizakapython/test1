54ebcd76529a6efbc7da2b6ffaf79307b86e8899
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2024-10-22 10:16:38 +0900

    Modefy20241022

---
+++ dotnet/test1/dotinstall1.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace test1{
+    int num;
+    class MyApp {
+        static void Main(){
+        
+            switch (num){
+                case 0:
+                    // そのまま
+                    break;
+                case 1:
+                    // 右へ
+                    break; 
+                case 2:
+                    //  左へ
+                    break;
+
+            }
+        }    
+    }
+}
