17be0b4c095ba2449da210343bb51d707a3f36bb
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2026-07-30 17:28:38 +0900

    Modefy20260730

--- JGPH_log/delete_log.ps1
+++ JGPH_log/delete_log.ps1
@@ -4,10 +4,14 @@
 ####   Author by takao.hattori        ###
 #########################################
 
+# JGPHツールのログファイルのディレクトリの格納先変数の定義
 $logpath = "C:\Users\takao.hattori\OneDrive - Accenture\JGPH_log"
+# JGPHツールのログファイルの7日前以前のファイル一覧を出力するファイル名変数の定義
 $result_csv = "C:\Users\takao.hattori\OneDrive - Accenture\JGPH_log\filelist.csv"
 
-$file_type = "*.csv"
+# 削除対象のファイルの拡張子変数の定義
+$file_type = "*.xlsx"
+
 
 cd $logpath
 
@@ -19,5 +23,14 @@ $OldLogs = Get-ChildItem -Path $logpath -Filter $file_type -File |
     Where-Object { $_.LastWriteTime -le $Threshold }
 
 # 結果を表示
-$OldLogs | Select-Object Name | Export-Csv -path $result_csv
+$OldLogs | Select-Object Name | Export-Csv -path $result_csv -Encoding UTF8 
 
+# ファイルを1行ずつ読み込む
+foreach ($line in Get-Content -Path $result_csv  -Encoding UTF8) {
+    # $lineの値が「#TYPE Selected.System.IO.FileInfo」かつ「"Name"」出ない場合、$line変数の中身を表示する
+    if( -not ($line -eq "#TYPE Selected.System.IO.FileInfo") -and ( -not ($line -eq '"Name"' ))) {
+         Write-Host $line
+        
+        
+    }
+}
