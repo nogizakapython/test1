51c71a8b2f2c76efc789485c98f0f0f37303e737
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2026-07-30 16:14:10 +0900

    Modefy20260730

---
+++ JGPH_log/delete_log.ps1
@@ -0,0 +1,23 @@
+﻿#########################################
+####   JGPHログ　ログ削除処理         ###
+####   新規作成   2026/7/30           ###
+####   Author by takao.hattori        ###
+#########################################
+
+$logpath = "C:\Users\takao.hattori\OneDrive - Accenture\JGPH_log"
+$result_csv = "C:\Users\takao.hattori\OneDrive - Accenture\JGPH_log\filelist.csv"
+
+$file_type = "*.csv"
+
+cd $logpath
+
+# 基準日（今日から7日前）
+$Threshold = (Get-Date).AddDays(-7)
+
+# 7日前より古いログファイルを取得
+$OldLogs = Get-ChildItem -Path $logpath -Filter $file_type -File |
+    Where-Object { $_.LastWriteTime -le $Threshold }
+
+# 結果を表示
+$OldLogs | Select-Object Name | Export-Csv -path $result_csv
+
