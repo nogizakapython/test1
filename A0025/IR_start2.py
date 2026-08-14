325b8b6e774cb530cfea63e593029aab44706c43
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2026-08-14 15:31:21 +0900

    Modefy20260814

--- A0025/IR_start2.py
+++ A0025/IR_start2.py
@@ -5,105 +5,111 @@
 ############  修正       2024/11/12 takao.hattori 高島屋のHPからスクレイピング不可のための修正
 ############  修正       2025/2/25 takao.hattori 三菱ふそう、矢崎総業を自動から手動へ変更に伴う修正
 ############  修正       2025/11/20 takumi.kurematsu 日産自動車の中期経営計画の発表用検索自動化ツールを新規組み込み
+############  修正       2026/8/14  takao.hattori    途中から実行する場合の説明をコメントに追記
 ##################################################
 
-# kessan_excel_copyと書かれている行に「#」を入れないでください
-
-import kessan_excel_copy
-import aeon
-import ajinomoto
-import ana
-import asahi
-import astellas
-import bridgestone
-import bristol
-import bat
-import chugai
-import cocacola
-import matsukiyo
-import daitokentaku
-import daiwahouse
-import denso
-import fastretailing
-import honda
-import isetan
-import jal
-import jtb
-import kao
-#import kubota
-#import lixil
-#import misumi
-#import mitsubishisyoji
-#import nipponham
-#import nipponexpress
-#import nitori
-#import nihonusen
-#import odakyu
-#import olcgroup
-#import omuron
-#import nissan
-#import nissan2
-#import nissan3
-#import ryohin
-#import santen
-#import santen2
-#import sevenandi
-#import shimano
-#import shionogi
-#import shiseido
-#import subaru
-#import toyota
-#import toyotabosyoku
-#import unicharm
+# kessan_excel_copyと書かれている行の「#」は削除しないでください。
+# 途中から実行する場合、検索済みの企業の行の文頭に「#」を入れてください
+# 検索対象の企業の文頭に「#」がないことを確認してください。文頭に「#」がある場合、「#」を削除してください。
 
 
-# kessan_excel_copyと書かれている行に「#」を入れないでください
+# import kessan_excel_copy
+# import aeon
+# import ajinomoto
+# import ana
+# import asahi
+# import astellas
+# import bridgestone
+# import bristol
+# import bat
+# import chugai
+# import cocacola
+# import matsukiyo
+# import daitokentaku
+# import daiwahouse
+# import denso
+# import fastretailing
+# import honda
+# import isetan
+# import jal
+# import jtb
+# import kao
+import kubota
+import lixil
+import misumi
+import mitsubishisyoji
+import nipponham
+import nipponexpress
+import nitori
+import nihonusen
+import odakyu
+import olcgroup
+import omuron
+import nissan
+import nissan2
+import nissan3
+import ryohin
+import santen
+import santen2
+import sevenandi
+import shimano
+import shionogi
+import shiseido
+import subaru
+import toyota
+import toyotabosyoku
+import unicharm
+
+
+# kessan_excel_copyと書かれている行にある「#」を削除しないでください(2026/8/12 takao.hattori)
+# 途中から実行する場合、検索済みの企業の行の文頭に「#」を入れてください
+# 検索対象の企業の文頭に「#」がないことを確認してください。文頭に「#」がある場合、「#」を削除してください。
 
 try:
-    exec(kessan_excel_copy)
-    exec(aeon)
-    exec(ajinomoto)
-    exec(ana)
-    exec(asahi)
-    exec(astellas)
-    exec(bridgestone)
-    exec(bristol)
-    exec(bat)
-    exec(chugai)
-    exec(cocacola)
-    exec(matsukiyo)
-    exec(daitokentaku)
-    exec(daiwahouse)
-    exec(denso)
-    exec(fastretailing)
-    exec(honda)
-    exec(isetan)
-    exec(jal)
-    exec(jtb)
-    exec(kao)
-    #exec(kubota)
-    #exec(lixil)
-    #exec(misumi)
-    #exec(mitsubishisyoji)
-    #exec(nipponham)
-    #exec(nipponexpress)
-    #exec(nitori)
-    #exec(nihonusen)
-    #exec(odakyu)
-    #exec(olcgroup)
-    #exec(omuron)
-    #exec(nissan)
-    #exec(nissan2)
-    #exec(nissan3)
-    #exec(ryohin)
-    #exec(santen)
-    #exec(santen2)
-    #exec(sevenandi)
-    #exec(shimano)
-    #exec(shionogi)
-    #exec(shiseido)
-    #exec(subaru)
-     exec(toyota)
+    # exec(kessan_excel_copy)
+    # exec(aeon)
+    # exec(ajinomoto)
+    # exec(ana)
+    # exec(asahi)
+    # exec(astellas)
+    # exec(bridgestone)
+    # exec(bristol)
+    # exec(bat)
+    # exec(chugai)
+    # exec(cocacola)
+    # exec(matsukiyo)
+    # exec(daitokentaku)
+    # exec(daiwahouse)
+    # exec(denso)
+    # exec(fastretailing)
+    # exec(honda)
+    # exec(isetan)
+    # exec(jal)
+    # exec(jtb)
+    # exec(kao)
+    exec(kubota)
+    exec(lixil)
+    exec(misumi)
+    exec(mitsubishisyoji)
+    exec(nipponham)
+    exec(nipponexpress)
+    exec(nitori)
+    exec(nihonusen)
+    exec(odakyu)
+    exec(olcgroup)
+    exec(omuron)
+    exec(nissan)
+    exec(nissan2)
+    exec(nissan3)
+    exec(ryohin)
+    exec(santen)
+    exec(santen2)
+    exec(sevenandi)
+    exec(shimano)
+    exec(shionogi)
+    exec(shiseido)
+    exec(subaru)
+    exec(toyota)
     exec(toyotabosyoku)
     exec(unicharm)
     
